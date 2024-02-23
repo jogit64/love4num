da038330cbe3d4d5c34d62b0c0b7e073f905c21f
jogit64 <usersfdc18@gmail.com>
2024-02-23 09:46:46 +0100

    version 1.0 - sans étoile - sans popu attraction

--- js/love4num.js
+++ js/love4num.js
@@ -1,8 +1,19 @@
 jQuery(document).ready(function ($) {
-  $("#generer-numeros").click(function () {
+  $(".game-option").click(function () {
+    var texte = $("#phrase-positive").val().trim(); // Utilisez .trim() pour enlever les espaces blancs avant et après le texte
+
+    if (texte === "") {
+      alert(
+        "Veuillez entrer une phrase ou des mots d'amour avant de générer des numéros."
+      ); // Affiche une alerte, ou vous pouvez choisir d'afficher le message différemment
+      return; // Sortie précoce de la fonction pour ne pas continuer avec la requête AJAX
+    }
+
+    var gameType = $(this).data("game"); // Récupère le type de jeu de l'attribut data-game
     var data = {
       action: "generer_numeros_loto",
-      texte: $("#phrase-positive").val(),
+      texte: texte,
+      jeu: gameType,
     };
     $.post(love4numAjaxUrl, data, function (response) {
       $("#resultats").html(response);
--- love4num_widget.php
+++ love4num_widget.php
@@ -23,13 +23,22 @@ add_action('wp_enqueue_scripts', 'love4num_scripts');
 
 function love4num()
 {
+    $lotoImgPath = plugin_dir_url(__FILE__) . 'assets/loto.png';
+    $euromillionsImgPath = plugin_dir_url(__FILE__) . 'assets/euromillions.png';
+    $eurodreamsImgPath = plugin_dir_url(__FILE__) . 'assets/dreams.png';
+
     $content = <<<HTML
 <div id="love4num-widget">
-    <h2>Votre Amour en Numéros de Loto</h2>
-    <p>Entrez une phrase ou des mots d'amour et voyez comment l'univers transforme votre message en numéros de loto chargés d'amour.</p>
+    <h2>Transformez votre amour en numéros de chance</h2>
+    <p>Entrez une phrase ou des mots d'amour pour voir comment l'univers transforme votre message en numéros de chance.</p>
     <div id="loto-widget">
         <input type="text" id="phrase-positive" placeholder="Entrez votre phrase positive">
-        <button id="generer-numeros">Générer Numéros</button>
+        <p>Choisissez le tirage pour générer vos numéros d'amour :</p>
+        <div class="game-selection">
+            <div class="game-option" data-game="loto"><img src="{$lotoImgPath}" alt="Loto"><span class="game-label">Loto</span></div>
+            <div class="game-option" data-game="euromillions"><img src="{$euromillionsImgPath}" alt="Euromillions"><span class="game-label">Euromillions</span></div>
+            <div class="game-option" data-game="eurodreams"><img src="{$eurodreamsImgPath}" alt="Eurodreams"><span class="game-label">Eurodreams</span></div>
+        </div>
     </div>
     <div id="resultats"></div>
 </div>
@@ -39,32 +48,64 @@ HTML;
 add_shortcode('love4num', 'love4num');
 
 
+
+
+
+
 function generer_numeros_loto()
 {
-    // Récupérer le texte de la requête
     $texte = isset($_POST['texte']) ? sanitize_text_field($_POST['texte']) : '';
+    if (empty($texte)) {
+        echo "Veuillez entrer une phrase ou des mots d'amour avant de générer des numéros.";
+        wp_die();
+    }
 
-    // Générer un seed à partir du texte
+    $jeu = isset($_POST['jeu']) ? $_POST['jeu'] : 'loto'; // Récupère le type de jeu
+
     $seed = crc32($texte);
     mt_srand($seed);
 
-    $numeros = array_rand(range(1, 49), 5);
-    $numeros_selectionnes = array_map(function ($index) {
-        return $index + 1; // Ajustement car array_rand retourne des clés
-    }, $numeros);
-    sort($numeros_selectionnes);
-    $numero_complementaire = rand(1, 10);
-
-    echo "<div class='titre'>Numéros</div>"; // Titre pour les numéros
-    // Boucle pour afficher les numéros
-    echo "<div>"; // Conteneur pour les numéros pour une meilleure gestion du style
-    foreach ($numeros_selectionnes as $num) {
-        echo "<span class='numero'>$num</span> "; // Afficher chaque numéro
+    switch ($jeu) {
+        case 'loto':
+            $keys = array_rand(range(1, 49), 5);
+            $numeros = array_map(function ($key) {
+                return $key + 1;
+            }, $keys);
+            $numero_complementaire = rand(1, 10);
+            $response = "<div class='titre'>Numéros Loto:</div>" .
+                "<div class='numeros loto-numeros'>" . implode('</div><div class="numeros loto-numeros">', $numeros) . "</div>" .
+                "<div class='numero-complementaire loto-complementaire'>$numero_complementaire</div>";
+            break;
+        case 'euromillions':
+            $keysNum = array_rand(range(1, 50), 5);
+            $numeros = array_map(function ($key) {
+                return $key + 1;
+            }, $keysNum);
+            $keysEtoiles = array_rand(range(1, 12), 2);
+            $etoiles = array_map(function ($key) {
+                return $key + 1;
+            }, $keysEtoiles);
+            $response = "<div class='titre'>Numéros Euromillions:</div>" .
+                "<div class='numeros euromillions-numeros'>" . implode('</div><div class="numeros euromillions-numeros">', $numeros) . "</div>" .
+                "<div class='etoiles euromillions-etoiles'>" . implode('</div><div class="etoiles euromillions-etoiles">', $etoiles) . "</div>";
+            break;
+        case 'eurodreams':
+            $keysNum = array_rand(range(1, 40), 6);
+            $numeros = array_map(function ($key) {
+                return $key + 1;
+            }, $keysNum);
+            $numeroDream = rand(1, 5);
+            $response = "<div class='titre'>Numéros Eurodreams:</div>" .
+                "<div class='numeros eurodreams-numeros'>" . implode('</div><div class="numeros eurodreams-numeros">', $numeros) . "</div>" .
+                "<div class='numero-dream eurodreams-dream'>$numeroDream</div>";
+            break;
     }
-    echo "</div>";
-    echo "<div class='titre'>Numéro Complémentaire</div><div><span class='numero'>$numero_complementaire</span></div>"; // Titre et affichage pour le numéro complémentaire
+
+    echo $response;
     wp_die();
 }
+
+
 
 // Hook pour les utilisateurs connectés
 add_action('wp_ajax_generer_numeros_loto', 'generer_numeros_loto');
